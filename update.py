22f81d4aa7874715bead8f92cb6b9803167187c0
Stelios Daveas <sdaveas@gmail.gr>
2020-02-12 23:58:37 +0000

    Add script to update changed dotfiles

---
+++ update.py
@@ -0,0 +1,22 @@
+import subprocess
+
+command = "find -type f"
+command_list = command.split(' ')
+
+out = subprocess.Popen(command_list,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+
+files = str(out.communicate()[0].decode('utf-8')).splitlines()
+
+home = '/home/stelios/'
+
+for versioned_file in files:
+    if versioned_file.startswith('./.git'):
+        continue
+    print(versioned_file)
+    home_file = home + versioned_file
+    command = ['rsync', home_file, versioned_file]
+    subprocess.Popen(command,
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.STDOUT)

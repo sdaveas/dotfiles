a8ee977742166a8511a279cae05f79272c21eb78
Stelios Daveas <sdaveas@iit.demokritos.gr>
2020-02-14 12:53:05 +0200

    Add whitespace

--- automatically_update_dotfiles.sh
+++ automatically_update_dotfiles.sh
@@ -14,7 +14,7 @@ else
                 git diff HEAD~1
                 echo "Commiting changes ...";
                 git add $(git diff --name-only)
-                read -p "Commit message: (default is Automatically commited changes)" message
+                read -p "Commit message: (default is Automatically commited changes) " message
                 message=${message:-"Automatically commited changes"}
                 echo $message
                 git commit -m "$message";

a03468503d67446351a8966beb8d0d67b5d5b458
Stelios Daveas <sdaveas@gmail.gr>
2020-02-26 22:56:04 +0000

    Add script to check if there are changes at dotfiles

---
+++ automate.py
@@ -0,0 +1,33 @@
+#!/bin/python
+
+"""
+Runs 'update.py' and checks if there were any changes
+"""
+import subprocess
+
+repo_path = '/home/stelios/etc/dotfiles'
+
+command = str('git -C ' + repo_path + ' status')
+command_list = command.split(' ')
+
+out = subprocess.Popen(command_list, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+git_status = str(out.communicate()[0].decode('utf-8')).splitlines()
+
+changed_files = []
+
+for l in git_status:
+    if 'modified' in l:
+        changed_files.append(l.split(':')[1].strip(' '))
+
+changed = len(changed_files)
+if changed <= 0:
+    exit()
+elif changed == 1:
+    print('There is a changed dotfile: ', end='')
+elif changed > 1:
+    print('There are', len(changed_files), 'changed dotfiles: ', end='')
+
+for c in changed_files:
+    print(c, end=' ')
+
+print("")

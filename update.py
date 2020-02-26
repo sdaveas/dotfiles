9271378d0329e34fbea975da7a72b49f76e7707d
Stelios Daveas <sdaveas@gmail.gr>
2020-02-26 23:32:06 +0000

    Make update.py prettier

--- update.py
+++ update.py
@@ -1,22 +1,25 @@
 #!/bin/python
 import subprocess
 
-command = "find -type f"
-command_list = command.split(' ')
+def update():
+    command = "find -type f"
+    command_list = command.split(' ')
 
-out = subprocess.Popen(command_list,
+    out = subprocess.Popen(command_list,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT)
+
+    files = str(out.communicate()[0].decode('utf-8')).splitlines()
+
+    home = '/home/stelios/'
+
+    for versioned_file in files:
+        if versioned_file.startswith('./.git'):
+            continue
+        home_file = home + versioned_file
+        command = ['rsync', home_file, versioned_file]
+        subprocess.Popen(command,
                          stdout=subprocess.PIPE,
                          stderr=subprocess.STDOUT)
 
-files = str(out.communicate()[0].decode('utf-8')).splitlines()
-
-home = '/home/stelios/'
-
-for versioned_file in files:
-    if versioned_file.startswith('./.git'):
-        continue
-    home_file = home + versioned_file
-    command = ['rsync', home_file, versioned_file]
-    subprocess.Popen(command,
-                     stdout=subprocess.PIPE,
-                     stderr=subprocess.STDOUT)
+update()
